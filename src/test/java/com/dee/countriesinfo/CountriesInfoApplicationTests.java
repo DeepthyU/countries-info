21cd9ca933d8ff76ddabee3998ae0a02a88ac820
Deepthy Unnikrishnan <mail.deepthyu@gmail.com>
2023-12-04 19:19:33 +0100

    CountriesInfo Application
    application files initial commit.

---
+++ src/test/java/com/dee/countriesinfo/CountriesInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dee.countriesinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
